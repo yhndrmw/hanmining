6982731f0899f676bef86a131de964f9ba6af115
TOP GLOBAL BOCIL <78413466+yhndrmw@users.noreply.github.com>
2021-02-03 05:59:01 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+//Tetep berkarya walaj gjls
+
+$ua = "Mozilla/5.0 (Linux; Android 5.1.1; SM-J320M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36";
+$Cookie = "__tawkuuid=e::captcha4doge.com::HSPq4vj37S98hNprLtp2/ECKbFeBcHDeXXJdQag3e6EClq/6q3bxzw3GKCJQ9/o4::2;PHPSESSID=9d943d76baf221d048c91c737a7a24e9";
+
+?>
